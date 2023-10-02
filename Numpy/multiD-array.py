a981d9ade6867c82df677331752fe1dc0c03cc2e
fredi4302 <fredigaming4302@gmail.com>
2023-10-02 18:39:50 +0530

    Update multiD-array.py
    added how to display array

--- Numpy/multiD-array.py
+++ Numpy/multiD-array.py
@@ -17,3 +17,11 @@ arr.shape
 
 #resize dimensions of an array
 arr.reshape(4,3)
+
+#how indexing takes place in 2D or more full matrix is displayed
+arr[:,:]
+
+#to print particular sequence do this
+#below code goes from row 0 to 2
+#column goes from 0 to 3
+arr[0:2,0:3]
