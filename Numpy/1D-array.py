76196b50b2b0086d8fd6d2e59d142196fa2afd46
fredi4302 <fredigaming4302@gmail.com>
2023-10-02 13:09:25 +0530

    Create 1D-array.py
    Adding and explaining features of
    1D-array

---
+++ Numpy/1D-array.py
@@ -0,0 +1,14 @@
+# import the numpy you can use np instead of writing numpy
+import numpy as np
+
+#converting a list into array using numpy
+l1=[1,12,123,1234]
+
+#array is innbuilt data type to convert data type to array
+arr=np.array(l1)
+
+#type function tells the type of datatype used
+
+#rhis functions tells us how many rows and columns are present in the array if it's 1-D array than it prints number of elements
+arr.shape
+
