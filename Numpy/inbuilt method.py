3469d4f77fb6dc6f4191a6209f48497092d82165
fredi4302 <fredigaming4302@gmail.com>
2023-10-05 09:55:13 +0530

    Create inbuilt method.py

---
+++ Numpy/inbuilt method.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+li=[5,58,90,1,658,32,12,60,0]
+
+#using arrange function to create 1D array
+ar=np.arange(0,10)
+
+#using arrange function to create 1D array incrementing the gap by using step function
+ar=np.arange(0,10,step=2)
+
+#to create an array of real number
+arr=np.linspace(1,10,50)
+
+#copy() function and brodcasting
